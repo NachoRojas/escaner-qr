84d3ccb7e62152b9af6444b4677437da867788fd
NachoRojas11 <116693879+NachoRojas11@users.noreply.github.com>
2024-07-23 21:00:25 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 window.addEventListener('load', () => {
-    const lectorCodigo = new ZXing.BrowserQRCodeReader();
+    const lectorCodigo = new ZXing.BrowserMultiFormatReader(); // Utilizamos BrowserMultiFormatReader para soportar múltiples formatos
     const elementoVistaPrevia = document.getElementById('vista-previa');
+    const elementoResultado = document.getElementById('resultado');
 
     lectorCodigo.getVideoInputDevices()
         .then(dispositivosEntradaVideo => {
-            const primerDispositivoId = dispositivosEntradaVideo[0].deviceId;
-            lectorCodigo.decodeFromVideoDevice(primerDispositivoId, 'vista-previa', (resultado, error) => {
+            let dispositivoFrontal = dispositivosEntradaVideo.find(dispositivo => dispositivo.label.toLowerCase().includes('front')) || dispositivosEntradaVideo[0];
+
+            lectorCodigo.decodeFromVideoDevice(dispositivoFrontal.deviceId, 'vista-previa', (resultado, error) => {
                 if (resultado) {
-                    if (resultado.text.startsWith('http')) {
-                        window.location.href = resultado.text;
-                    } else {
-                        console.log('Contenido del QR:', resultado.text);
+                    if (resultado.format === ZXing.BarcodeFormat.QR_CODE) {
+                        if (resultado.text.startsWith('http')) {
+                            window.location.href = resultado.text;
+                        } else {
+                            console.log('Contenido del QR:', resultado.text);
+                        }
+                    } else if (resultado.format === ZXing.BarcodeFormat.ITF) {
+                        if (/^\d+$/.test(resultado.text)) { // Verificar si el contenido es numérico
+                            elementoResultado.textContent = `Código de barras: ${resultado.text}`;
+                        } else {
+                            console.log('Código de barras Interleave 2 of 5:', resultado.text);
+                        }
                     }
                 }
                 if (error && !(error instanceof ZXing.NotFoundException)) {
